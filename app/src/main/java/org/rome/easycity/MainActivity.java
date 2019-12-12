c2bf3818d9f29c8d31171b114c3812b3fb12e293
Internet of Spyrus <avlonitiss@yahoo.co.uk>
2019-12-13 01:42:19 +0200

    maps second activity

--- app/src/main/java/org/rome/easycity/MainActivity.java
+++ app/src/main/java/org/rome/easycity/MainActivity.java
@@ -1,30 +1,73 @@
 package org.rome.easycity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.view.View;
+import androidx.annotation.NonNull;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
+
+    private LoginButton loginButton;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+    private CallbackManager mCallbackManager;
+    private static final String EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mCallbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
-
-
-
+// ...
+// Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            setContentView(R.layout.activity_easy_city_maps);
+        }
     }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       if(currentUser!=null){
+           setContentView(R.layout.activity_easy_city_maps);
+       }
+    }
+
+
 }
