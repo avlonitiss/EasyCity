5ec599bf772af355817c54522fb25dd018fc8b97
Administrator <avlonitiss@yahoo.co.uk>
2019-12-12 17:13:40 +0200

    build on job pc

--- app/src/main/java/org/rome/easycity/MainActivity.java
+++ app/src/main/java/org/rome/easycity/MainActivity.java
@@ -2,15 +2,29 @@ package org.rome.easycity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 }
