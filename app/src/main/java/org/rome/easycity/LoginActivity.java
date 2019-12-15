272f0db61ba12abb0ba37c3eb2cf3a4839307483
Internet of Spyrus <avlonitiss@yahoo.co.uk>
2019-12-16 00:48:45 +0200

    login check

--- app/src/main/java/org/rome/easycity/LoginActivity.java
+++ app/src/main/java/org/rome/easycity/LoginActivity.java
@@ -1,19 +1,28 @@
 package org.rome.easycity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button LoginButton;
     private EditText UserEmail, UserPassword;
     private TextView NeedNewAccountLink;
+    private FirebaseAuth mAuth;
 
 
 
@@ -25,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         NeedNewAccountLink = (TextView) findViewById(R.id.register_account_link);
         UserEmail = (EditText) findViewById(R.id.login_email);
         UserPassword = (EditText) findViewById(R.id.login_password);
+        mAuth = FirebaseAuth.getInstance();
 
         NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +42,45 @@ public class LoginActivity extends AppCompatActivity {
                 SendUserToRegisterActivity();
             }
         });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowingUserToLogin();
+            }
+        });
+    }
+
+    private void AllowingUserToLogin() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Παρακαλώ δώστε ...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Παρακαλώ δώστε password ...", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "Συνδεθήκατε ...", Toast.LENGTH_SHORT).show();
+                            }
+
+                            else {
+
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Κάτι πήγε στραβά "+message, Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }
     }
 
     private void SendUserToRegisterActivity() {
